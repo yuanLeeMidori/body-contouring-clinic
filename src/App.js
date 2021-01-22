2629031cc0de8df7d6216bdc366bbbad4ccc2b78
yuanLeeMidori <midori3113133@gmail.com>
2021-01-21 22:25:41 -0500

    Add: menubar, appointment home, basic router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, FormGroup, Form, FormControl, Grid, Row, Col, Button } from 'react-bootstrap'
+import { Link, Switch, Redirect, Route, BrowserRouter } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'  // temporary, we can create our css later
+import AppointmentHome from './Appointment/AppointmentHome'
+import logo from './brand-logo.png';
 
 class App extends React.Component{
 
@@ -19,15 +24,51 @@ class App extends React.Component{
 
   render(){
     return(
-          <div className="App">
-            <header className="App-header">
-              <p>
-                Body Contouring Clinic <br/>
-                React : true <br/>
-                {`Express : ${this.state.backServer}`}
-              </p>
-            </header>
-          </div>
+      <div className="App">
+        <nav class="nav-back navbar navbar-expand-lg navbar-light">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link">BodyContouringClinic@gmail.com</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link">(416) 966 - 0006</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/services">Service and Prices</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/Appointment">Appointment</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/Request">Request</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/Profile">Profile</a>
+            </li>
+          </ul>
+        </nav>
+        <br />
+        <img src={logo} alt="logo" />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' render={() =>
+                <header className="App-basic">
+                    <p>
+                      Body Contouring Clinic <br/>
+                      React : true <br/>
+                      {`Express : ${this.state.backServer}`}
+                    </p>
+                </header>} />
+            <Route exact path='/Appointment' render={() =>
+            <AppointmentHome />} />
+          </Switch>
+        </BrowserRouter>
+        </div>
     );
   }
 }

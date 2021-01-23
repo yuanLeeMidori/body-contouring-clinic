d4c34e8d747665b71c01b8b5a3efe3d6bcf2ca7f
Eunbee Kim <dmsql1678@gmail.com>
2021-01-23 12:58:40 -0500

    issue-18: Moved SideBar to SideBar directory in order to use it globally

--- src/Request/index.js
+++ src/Request/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SideBar from './SideBar';
+import SideBar from '../SideBar/SideBar';
 import RequestHome from './RequestHome';
 
 class index extends React.Component {

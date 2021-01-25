849b72b82d7b02d76f6a214a672b889f12144f24
eunbeek <50673489+eunbeek@users.noreply.github.com>
2021-01-25 00:58:55 -0500

    Update AnswerRequest.js

--- src/Request/Admin/AnswerRequest.js
+++ src/Request/Admin/AnswerRequest.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SideBar from '../SideBar/SideBar';
+import SideBar from '../../SideBar/SideBar';
 
 class AnswerRequest extends React.Component {
 

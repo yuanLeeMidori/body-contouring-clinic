57da195b8abafd562c9036302b4b51f0177e27fb
Mintae Kim <sonechca1@naver.com>
2021-01-27 17:27:28 -0500

    Improve design and Edit contents of Customer page

--- src/Customer/CustomerProfile.js
+++ src/Customer/CustomerProfile.js
@@ -1,67 +1,87 @@
-import React from 'react';
-import '../App.css';
-import SideBar from '../SideBar/SideBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "../App.css";
+import SideBar from "../SideBar/SideBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
 
 class CustomerProfile extends React.Component {
-    state = {
-        items: [
-            {url:'/Customer/', title: 'Home'},
-            {url:'/Customer/Profile', title: 'Profile'},
-            {url:'/Customer/Edit', title: 'Edit Profile'},
-        ]
-    }
-    constructor(prop){
-        super(prop)
-    }
+  state = {
+    items: [
+      { url: "/Customer/", title: "Home" },
+      { url: "/Customer/Profile", title: "Profile" },
+      { url: "/Customer/Edit", title: "Edit Profile" },
+    ],
+  };
+  constructor(prop) {
+    super(prop);
+  }
 
-    render() {
-        
-        return (
-            <div className="App-basic" class="form-group">
-                <div class="row">
-                    <SideBar items={this.state.items}/>
-                    <div className="col-md-7">
-                        <h1>Hello, user.fullName</h1>
-                        <hr />
-
-                        <h2>Profile</h2>
-                        <br />
-                        <form>
-                            <label>
-                                First Name: 
-                                <input type="text" name="fullName" value={"Default"}/>
-                            </label><br />
-                            <label>
-                                last Name: 
-                                <input type="text" name="fullName" value={"Default"}/>
-                            </label><br />
-                            <label>
-                                New password: 
-                                <input type="password" name="fullName" />
-                            </label><br />
-                            <label>
-                                Confirm Password: 
-                                <input type="password" name="fullName" />
-                            </label><br />
-                            <label>
-                                Email: 
-                                <input type="email" name="fullName" />
-                            </label><br />
-                            <label>
-                                Contract Number: 
-                                <input type="text" name="fullName" />
-                            </label><br />
-                            <br />
-                            <button type="submit">Submit</button>
-                        </form>
-                    </div>
-
-                </div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+        <div className="row">
+        <div className="col-md-1"></div>
+        <SideBar items={this.state.items}/>
+        <div class="col-md-6" style={{'margin-left':'80px'}}>
+            <h2 className="PageTitle">Profile</h2><hr/><br/>
+            <Container class="col-md-6">
+            <Form style={{'fontSize': '20px', 'marginLeft':'80px', 'textAlign' : 'left'}}>
+              <Form.Group as={Row}>
+                <Form.Label column md={3}>
+                  Name: 
+                </Form.Label>
+                <Col sm={1}>
+                  <Form.Label column md={0} >
+                      User.FullName
+                  </Form.Label>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Email:
+                </Form.Label>
+                <Col sm={1}>
+                  <Form.Label column md={0}>
+                      User.Email
+                  </Form.Label>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Address:
+                </Form.Label>
+                <Col sm={1}>
+                  <Form.Label column md={0}>
+                      User.Address
+                  </Form.Label>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Current Balance:
+                </Form.Label>
+                <Col sm={1}>
+                  <Form.Label column md={0}>
+                      User.Balance
+                  </Form.Label>
+                </Col>
+              </Form.Group>
+              <Form.Group as={Row}>
+                <Form.Label column sm={3}>
+                  Your level:
+                </Form.Label>
+                <Col sm={1}>
+                  <Form.Label column md={0}>
+                      User.level
+                  </Form.Label>
+                </Col>
+              </Form.Group>
+            </Form>
+          </Container>
+            <br/>
+        </div>
+    </div>
+    );
+  }
 }
 
-export default CustomerProfile
+export default CustomerProfile;

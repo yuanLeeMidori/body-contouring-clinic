ae7239aab104943454f3f7dd2fca24ac87623467
Eunbee Kim <dmsql1678@gmail.com>
2021-01-26 22:26:05 -0500

    issue-47: updated FAQ list style

--- src/Request/FAQ/ViewAllFAQ.js
+++ src/Request/FAQ/ViewAllFAQ.js
@@ -3,7 +3,7 @@ import '../../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ViewFAQ from './ViewFAQ';
 import ViewFAQbyAdmin from '../Admin/ViewFAQbyAdmin';
-import { Button, Container, Row, Col, Card, Nav,Tabs, Tab, TabContent } from 'react-bootstrap';
+import { ListGroup, Container, Row, Col, Card, Nav,Tabs, Tab, TabContent } from 'react-bootstrap';
 
 class ViewAllFAQ extends React.Component {
 
@@ -35,26 +35,34 @@ class ViewAllFAQ extends React.Component {
                                 onSelect={key => this.setState({ key })}
                             >
                                 <Tab eventKey="shipping" title="Shipping" style={{color:'#393F44', 'margin-top':'10px'}}>
-                                    <Card.Text onClick={this.showFAQ}>What are the shipping fee and delivery timeline?</Card.Text>
-                                    <Card.Text onClick={this.showFAQ}>What does in-stock, backordered and made-to-order mean?</Card.Text>
-                                    <Card.Text onClick={this.showFAQ}>What is the last date to place an order to receive by Dec 24th?</Card.Text>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item action onClick={this.showFAQ}>What are the shipping fee and delivery timeline?</ListGroup.Item>
+                                        <ListGroup.Item action onClick={this.showFAQ}>What does in-stock, backordered and made-to-order mean?</ListGroup.Item>
+                                        <ListGroup.Item action onClick={this.showFAQ}>What is the last date to place an order to receive by Dec 24th?</ListGroup.Item>
+                                    </ListGroup>
                                 </Tab>
                                 <Tab eventKey="myOrder" title="My Order" style={{color:'#393F44', 'margin-top':'10px'}}>
-                                    <Card.Text onClick={this.showFAQ}>What are the shipping fee and delivery timeline?</Card.Text>
-                                    <Card.Text onClick={this.showFAQ}>What does in-stock, backordered and made-to-order mean?</Card.Text>
-                                    <Card.Text onClick={this.showFAQ}>What is the last date to place an order to receive by Dec 24th?</Card.Text>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item action onClick={this.showFAQ}>What are the shipping fee and delivery timeline?</ListGroup.Item>
+                                        <ListGroup.Item action onClick={this.showFAQ}>What does in-stock, backordered and made-to-order mean?</ListGroup.Item>
+                                        <ListGroup.Item action onClick={this.showFAQ}>What is the last date to place an order to receive by Dec 24th?</ListGroup.Item>
+                                    </ListGroup>
                                 </Tab>
                                 <Tab eventKey="payment" title="Payment"  style={{color:'#393F44', 'margin-top':'10px'}}>
-                                    <Card.Text onClick={this.showFAQ}>How do I find my order number?</Card.Text>
-                                    <Card.Text>Can I cancel my order?</Card.Text>
-                                    <Card.Text>Can I update my order?</Card.Text>
-                                    <Card.Text>Can I change my address?</Card.Text>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item action onClick={this.showFAQ}>How do I find my order number?</ListGroup.Item>
+                                        <ListGroup.Item action onClick={this.showFAQ}>Can I cancel my order?</ListGroup.Item>
+                                        <ListGroup.Item action onClick={this.showFAQ}>Can I update my order?</ListGroup.Item>
+                                        <ListGroup.Item action onClick={this.showFAQ}>Can I change my address?</ListGroup.Item>
+                                    </ListGroup>
                                 </Tab>
                                 <Tab eventKey="returns" title="Returns"  style={{color:'#393F44', 'margin-top':'10px'}}>
-                                    <Card.Text onClick={this.showFAQ}>How do I find my order number?</Card.Text>
-                                    <Card.Text>Can I cancel my order?</Card.Text>
-                                    <Card.Text>Can I update my order?</Card.Text>
-                                    <Card.Text>Can I change my address?</Card.Text>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item action onClick={this.showFAQ}>How do I find my order number?</ListGroup.Item>
+                                        <ListGroup.Item action onClick={this.showFAQ}>Can I cancel my order?</ListGroup.Item>
+                                        <ListGroup.Item action onClick={this.showFAQ}>Can I update my order?</ListGroup.Item>
+                                        <ListGroup.Item action onClick={this.showFAQ}>Can I change my address?</ListGroup.Item>
+                                    </ListGroup>
                                 </Tab>
                             </Tabs>
                         </Container>

16e4259f4794a45450de0d14ac702dcfd5bcb764
yuanLeeMidori <midori3113133@gmail.com>
2021-01-29 23:54:25 -0500

    Modify css and add routing to Appointment, Appointments, and CreateAppointment

--- src/Appointment/Appointment.js
+++ src/Appointment/Appointment.js
@@ -3,13 +3,25 @@ import { Container, Row, Col } from 'react-bootstrap';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../app.module.css';
+import SideBar from '../SideBar/SideBar';
 
 class Appointment extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            items: [
+                { url: '/Appointment', title: 'Appointment Home' },
+                { url: '/Appointment/Appointments', title: 'View All Appointments' },
+                { url: '/Appointment/Create', title: 'Create Appointment' },
+            ]
+        };
     }
 
+    componentDidMount() {
+        document.title = "Create New Appointment | Body Contouring Clinic";
+    }
+    
     render() {
         const title = {
             color: '#393D47',
@@ -21,49 +33,54 @@ class Appointment extends React.Component {
         }
         return (
             <>
-                <h2 style={title}>Appointment Details</h2>
-                <Container>
-                    <Row>
-                        <Col></Col>
-                        <Col xs={7}>
-                        <table className={styles.appointmentTable}>
-                                <tr>
-                                    <td>Customer Name: </td>
-                                    <td>Jane Doe</td>
-                                </tr>
-                                <tr>
-                                    <td>Date:</td>
-                                    <td>2021/01/24</td>
-                                </tr>
-                                <tr>
-                                    <td>Time:</td>
-                                    <td>13:00-14:00</td>
-                                </tr>
-                                <tr>
-                                    <td>Technician:</td>
-                                    <td>John Doe</td>
-                                </tr>
-                                <tr>
-                                    <td>Service:</td>
-                                    <td>Laser-Any Body area</td>
-                                </tr>
-                                <tr>
-                                    <td>Contact #:</td>
-                                    <td>(437)988-1678</td>
-                                </tr>
-                                <tr>
-                                    <td>Special Request:</td>
-                                </tr>
-                            </table>
-                        </Col>
-                        <Col></Col>
-                    </Row>
-                    <Row>
-                        <Col></Col>
-                        <Col><a style={button}>Delete </a><a style={button}> Edit</a></Col>
-                    </Row>
-                </Container>
-
+                <div className="row">
+                <div className="col-md-1"></div>
+                <SideBar items={this.state.items} />
+                <div className="col-md-6">    
+                    <h2>Appointment Details</h2>
+                    <Container>
+                        <Row>
+                            <Col></Col>
+                            <Col xs={7}>
+                            <table className={styles.appointmentTable}>
+                                    <tr>
+                                        <td>Customer Name: </td>
+                                        <td>Jane Doe</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Date:</td>
+                                        <td>2021/01/24</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Time:</td>
+                                        <td>13:00-14:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Technician:</td>
+                                        <td>John Doe</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Service:</td>
+                                        <td>Laser-Any Body area</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Contact #:</td>
+                                        <td>(437)988-1678</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Special Request:</td>
+                                    </tr>
+                                </table>
+                            </Col>
+                            <Col></Col>
+                        </Row>
+                        <Row>
+                            <Col></Col>
+                            <Col><a style={button}>Delete </a><a style={button} href="/Appointment/Edit"> Edit</a></Col>
+                        </Row>
+                    </Container>
+                </div>
+                </div>
             </>
         )
     }
--- src/Appointment/CreateAppointment.js
+++ src/Appointment/CreateAppointment.js
@@ -1,101 +1,108 @@
-import React from 'react';
-import '../App.css';
-import { Form, Row, Col, Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import SideBar from '../SideBar/SideBar';
+import React from "react";
+import "../App.css";
+import { Form, Row, Col, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SideBar from "../SideBar/SideBar";
+import styles from "../app.module.css";
 
 class CreateAppointment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { url: "/Appointment", title: "Appointment Home" },
+        { url: "/Appointment/Appointments", title: "View All Appointments" },
+        { url: "/Appointment/Create", title: "Create Appointment" },
+      ],
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [
-                { url: '/Appointment', title: 'Appointment Home' },
-                { url: '/Appointment/Appointments', title: 'View All Appointments' },
-                { url: '/Appointment/Create', title: 'Create Appointment' },
-            ]
-        };
-    }
+  componentDidMount() {
+    document.title = "Create New Appointment | Body Contouring Clinic";
+  }
+  render() {
+    const title = {
+      margin: "5px 600px",
+      color: "#393D47",
+    };
+    return (
+      <>
+        <br />
+        <br />
+        <div className="row">
+          <div className="col-md-1"></div>
+          <SideBar items={this.state.items} />
+          <div className="col-md-6">
+            <h2 className={styles.appointmentTitle}>New Appointment</h2>
+            <Container>
+              <Row>
+                <Col></Col>
+                <Col xs={8}>
+                  <Form>
+                    <Form.Group as={Row} controlId="exampleForm.ControlSelect1">
+                      <Form.Label column sm="4">
+                        Services:
+                      </Form.Label>
+                      <Col sm="8">
+                        <Form.Control as="select">
+                          <option>Active air oxygen therapy</option>
+                          <option>Green peel</option>
+                          <option>Skin rejuventation</option>
+                          <option>laser hair removal</option>
+                        </Form.Control>
+                      </Col>
+                    </Form.Group>
 
-    componentDidMount() {
-        document.title = "Create New Appointment | Body Contouring Clinic";
-    }
-    render() {
-        const title = {
-            margin: '5px 600px',
-            color: '#393D47',
-        }
-        return (
-            <>
-                <br /><br />
-                <div class="row">
-                <SideBar items={this.state.items} />
-                <h2 style={title}>New Appointment</h2>
-                </div>
-                <Container>
-                    <Row>
-                        <Col></Col>
-                        <Col xs={8}>
-                        <Form>
-                            <Form.Group as={Row} controlId="exampleForm.ControlSelect1">
-                                <Form.Label column sm="4">
-                                Services:
-                                </Form.Label>
-                                <Col sm="8">
-                                        <Form.Control as="select">
-                                            <option>Active air oxygen therapy</option>
-                                            <option>Green peel</option>
-                                            <option>Skin rejuventation</option>
-                                            <option>laser hair removal</option>
-                                        </Form.Control>                                        
-                                </Col>
-                            </Form.Group>
+                    <Form.Group as={Row} controlId="exampleForm.ControlSelect1">
+                      <Form.Label column sm="4">
+                        Technician:
+                      </Form.Label>
+                      <Col sm="8">
+                        <Form.Control as="select">
+                          <option>Piper Chapman</option>
+                          <option>Alex Vause</option>
+                          <option>Daya Diaz</option>
+                          <option>Tasha Jefferson</option>
+                        </Form.Control>
+                      </Col>
+                    </Form.Group>
 
-                            <Form.Group as={Row} controlId="exampleForm.ControlSelect1">
-                                <Form.Label column sm="4">
-                                Technician:
-                                </Form.Label>
-                                <Col sm="8">
-                                        <Form.Control as="select">
-                                            <option>Piper Chapman</option>
-                                            <option>Alex Vause</option>
-                                            <option>Daya Diaz</option>
-                                            <option>Tasha Jefferson</option>
-                                        </Form.Control>        
-                                </Col>
-                            </Form.Group>
-                                
-                            <Form.Group as={Row}>
-                                <Form.Label column sm="4">
-                                    Date & Time:
-                                </Form.Label>
-                                <Col sm="8">
-                                    <Form.Control />
-                                </Col>
-                            </Form.Group>
-                            <Form.Group as={Row} controlId="exampleForm.ControlTextarea1">
-                                <Form.Label column sm="4">
-                                    Special Request:
-                                </Form.Label>        
-                                <Col sm="8">
-                                    <Form.Control as="textarea" rows={3} />
-                                </Col>
-                            </Form.Group>
-                        </Form>
-                        </Col>
-                        <Col></Col>
-                    </Row>
-                </Container>
-                <Container style={{'margin-top': '50px', cursor: 'pointer'}}>
-                    <Row >
-                        <Col></Col>
-                        <Col md="auto"><a>Cancel</a></Col>
-                        <Col lg="2">Save</Col>
-                    </Row>
-                </Container>
-            </>
-        )
-    }
+                    <Form.Group as={Row}>
+                      <Form.Label column sm="4">
+                        Date & Time:
+                      </Form.Label>
+                      <Col sm="8">
+                        <Form.Control />
+                      </Col>
+                    </Form.Group>
+                    <Form.Group
+                      as={Row}
+                      controlId="exampleForm.ControlTextarea1"
+                    >
+                      <Form.Label column sm="4">
+                        Special Request:
+                      </Form.Label>
+                      <Col sm="8">
+                        <Form.Control as="textarea" rows={3} />
+                      </Col>
+                    </Form.Group>
+                  </Form>
+                </Col>
+                <Col></Col>
+              </Row>
+            </Container>
+            <Container style={{ "margin-top": "50px", cursor: "pointer" }}>
+              <Row>
+                <Col></Col>
+                <Col md="auto"><a href="/Appointment">Cancel</a></Col>
+                <Col lg="2"><a href="/Appointment/Appointment">Save</a></Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default CreateAppointment;

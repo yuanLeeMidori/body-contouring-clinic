a8a134ada0155698cc9def40dc13eb7ac30f408b
yuanLeeMidori <midori3113133@gmail.com>
2021-01-29 23:09:23 -0500

    Add the rest of appointment pages

---
+++ src/Appointment/Admin/LeaveMessageToAppointment.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from '../app.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+class LeaveMessageToAppointment extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default LeaveMessageToAppointment;

a49ec9bb98f71401d70f66641ab899ac51f3b248
rko7 <hazel.r.ko@gmail.com>
2021-01-31 00:19:48 -0500

    SignUp.js updated(validation not working) + Image folder uploaded

--- src/Register/signUp.js
+++ src/Register/signUp.js
@@ -1,128 +1,292 @@
 import React, { Component } from "react";
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Row, Col, Container, Button } from 'react-bootstrap';
+import { Textbox, Textarea, Radiobox, Checkbox, Select } from 'react-inputs-validation';
+import 'react-inputs-validation/lib/react-inputs-validation.min.css';
+
 
  class SignUp extends Component {
+    componentDidMount(){
+        console.log(this.props);
+      }
+
     constructor(props) {
         super(props);
         this.state = {
-            id: null,
-            password: null,
-            firstName: null,
-            lastName: null,
-            email: null,
-            contactNumber: null,
-          errors: {
-            id: '',
-            password: '',
-            firstName: '',
-            lastName: '',
-            email: '',
-            contactNumber: '',          
-          }
-        };
-      }
-
-      handleChange = (event) => {
-        event.preventDefault();
-        const { name, value } = event.target;
-        let errors = this.state.errors;
-      
-        switch (name) {
-            case 'id': 
-            errors.id = 
-              value.length <= 25
-                ? 'ID must be less than 25 characters!'
-                : '';
-            break;
-            case 'password': 
-            errors.password = 
-              value.length <= 45
-                ? 'Password must be less than 45 characters!'
-                : '';
-            break;
-            case 'firstName': 
-            errors.firstName = 
-              value.length <= 45
-                ? 'First Name must be less than 45 characters!'
-                : '';
-            break;
-            case 'lastName': 
-            errors.lastName = 
-              value.length <= 45
-                ? 'Full Name must be less than 45 characters!'
-                : '';
-            break;
-            case 'email': 
-            errors.email = 
-              validEmailRegex.test(value)
-                ? ''
-                : 'Email is not valid';
-            break;
-            case 'contactNumber': 
-            errors.contactNumber = 
-              value.pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                ? 'Contact number must be 10 digit number with '-'!'
-                : '';
-            break;
-
-          default:
-            break;
+            input: {},
+            errors: {}
+          };
+           
+          this.handleChange = this.handleChange.bind(this);
+          this.handleSubmit = this.handleSubmit.bind(this);
         }
+
+      handleChange(event)  {
+        let input = this.state.input;
+        input[event.target.name] = event.target.value;
       
-        this.setState({errors, [name]: value}, ()=> {
-            console.log(errors)
-        })
+        this.setState({
+          input
+        });
       }
+         
+      handleSubmit(event) {
+        event.preventDefault();
+      
+        if(this.validate()){
+            console.log(this.state);
+      
+            let input = {};
+            input["id"] = "";
+            input["password"] = "";
+            input["confirmPassword"] = "";
+            input["firstName"] = "";
+            input["lastName"] = "";
+            input["email"] = "";
+            input["contactNumber"] = "";
+            this.setState({input:input});
+
+            alert('Sign-up Form is submited');
+
+        }
+      }
+      
+      validate(){
+          let input = this.state.input;
+          let errors = {};
+          let isValid = true;
+      
+ 
+          if (!input["id"]) {
+            isValid = false;
+            errors["id"] = "Please enter your user id.";
+          }
+
+          if (typeof input["id"] !== "undefined") {
+            var pattern = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{2,}$/)
+            if (!pattern.test(input["id"])) {
+                isValid = false;
+                errors["id"] = "Please enter valid user ID. User Id need at least one letter, one number with minimum 8 characters.";
+              }
+            } 
+ 
+          if (!input["password"]) {
+            isValid = false;
+            errors["password"] = "Please enter your password.";
+          }
+
+          if (typeof input["password"] !== "undefined") {
+            var pattern = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{12,}$/)
+            if (!pattern.test(input["id"])) {
+                isValid = false;
+                errors["password"] = "Please enter valid user ID. User Id need at least one letter, one number with minimum 12 characters.";
+              }
+            } 
+      
+          if (!input["confirmPassword"]) {
+            isValid = false;
+            errors["confirmPassword"] = "Please enter your confirm password.";
+          }
+      
+          if (typeof input["password"] !== "undefined" && typeof input["confirm_password"] !== "undefined") {
+              
+            if (input["password"] != input["confirm_password"]) {
+              isValid = false;
+              errors["password"] = "Passwords don't match.";
+            }
+          } 
+
+          if (!input["firstName"]) {
+            isValid = false;
+            errors["firstName"] = "Please enter your first name.";
+          }
+          if (typeof input["firstName"] !== "undefined") {
+            var pattern = new RegExp(/[a-zA-Z]/)
+            if (!pattern.test(input["firstName"])) {
+                isValid = false;
+                errors["firstName"] = "Please enter valid email address.";
+              }
+            }          
+
+          if (!input["lastName"]) {
+            isValid = false;
+            errors["lastName"] = "Please enter your last name.";
+          }
+          if (typeof input["lastName"] !== "undefined") {
+            var pattern = new RegExp(/[a-zA-Z]/)
+            if (!pattern.test(input["lastName"])) {
+                isValid = false;
+                errors["lastName"] = "Please enter valid last name.";
+              }
+            } 
+      
+          if (!input["email"]) {
+            isValid = false;
+            errors["email"] = "Please enter your email Address.";
+          }
+      
+          if (typeof input["email"] !== "undefined") {
+              
+            var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+            if (!pattern.test(input["email"])) {
+              isValid = false;
+              errors["email"] = "Please enter valid email address.";
+            }
+          }
+
+          if (!input["contactNumber"]) {
+            isValid = false;
+            errors["contactNumber"] = "Please enter your contact number.";
+          }
+      
+          if (typeof input["contactNumber"] !== "undefined") {
+              
+            var pattern = new RegExp(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)
+            if (!pattern.test(input["contactNumber"])) {
+                isValid = false;
+                errors["contactNumber"] = "Please enter valid contact number.";
+              }
+            }
+
+          this.setState({
+            errors: errors
+          });
+      
+          return isValid;
+      }
+
+
 
     render() {
         return (
-            <form>
-            <h3>Login Form</h3>
+        <div className="row">
+          <div class="col-md-8" style={{'margin-left':'290px'}}>
+          <h2 className="PageTitle" style={{'margin-left': '390px'}}>Sign-up Form</h2><br/>
+          <Container style={{'margin-left': '190px'}}>
+            <Form>
+            <Form.Group as={Row}>
+                        <Form.Label column sm={2}>User ID:</Form.Label>
+                        <Col sm={4}>
+                            <Form.Control                             
+                            type="text" 
+                            name="id"
+                            minLength='2'
+                            maxLength='25'
+                            value={this.state.input.name}
+                            onChange={this.handleChange}
+                            placeholder="Enter user ID"
+                            id="id" 
+                            required></Form.Control>
+                        </Col>
+                        <div className="text-danger">{this.state.errors.id}</div>
+                    </Form.Group>            
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>Password:</Form.Label>
+                        <Col sm={4}>
+                            <Form.Control                             
+                            type="password" 
+                            name="password"
+                            minLength='8'
+                            maxLength='45'
+                            value={this.state.input.name}
+                            onChange={this.handleChange}
+                            placeholder="Enter Password"
+                            id="password" 
+                            required></Form.Control>
+                        </Col>
+                        <div className="text-danger">{this.state.errors.password}</div>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>Password:</Form.Label>
+                        <Col sm={4}>
+                            <Form.Control                             
+                            type="password" 
+                            name="confirmPassword"
+                            minLength='8'
+                            maxLength='45'
+                            value={this.state.input.name}
+                            onChange={this.handleChange}
+                            placeholder="Re-enter the Password"
+                            id="confirmPassword" 
+                            required></Form.Control>
+                        </Col>
+                        <div className="text-danger">{this.state.errors.confimrPassword}</div>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>First Name:</Form.Label>
+                        <Col sm={4}>
+                            <Form.Control                             
+                            type="text" 
+                            name="firstName"
+                            value={this.state.input.name}
+                            onChange={this.handleChange}
+                            placeholder="Enter First name"
+                            id="firstName" 
+                            required></Form.Control>
+                        </Col>
+                        <div className="text-danger">{this.state.errors.firstName}</div>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>Last Name:</Form.Label>
+                        <Col sm={4}>
+                            <Form.Control                             
+                            type="text" 
+                            name="lastName"
+                            value={this.state.input.name}
+                            onChange={this.handleChange}
+                            placeholder="Enter Last name"
+                            id="lastName" 
+                            required></Form.Control>
+                        </Col>
+                        <div className="text-danger">{this.state.errors.lastName}</div>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>Email address:</Form.Label>
+                        <Col sm={4}>
+                            <Form.Control                             
+                            type="email" 
+                            name="email"
+                            value={this.state.input.name}
+                            onChange={this.handleChange}
+                            placeholder="Enter E-mail address"
+                            id="email" 
+                            required></Form.Control>
+                        </Col>
+                        <div className="text-danger">{this.state.errors.email}</div>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>Contact Number:</Form.Label>
+                        <Col sm={4}>
+                            <Form.Control                             
+                            type="tel" 
+                            name="contactNumber"
+                            value={this.state.input.name}
+                            onChange={this.handleChange}
+                            placeholder="Enter Contact number"
+                            id="contactNumber" 
+                            required></Form.Control>
+                        </Col>
+                        <div className="text-danger">{this.state.errors.contactNumber}</div>
+                    </Form.Group>
+              </Form>
+              </Container>
 
-            <div className="form-group">
-                <label>ID</label>
-                <input type="text" className="form-control" placeholder="ID" />
-            </div>
-
-            <div className="form-group">
-                <label>Password</label>
-                <input type="password" className="form-control" placeholder="Enter password" />
-            </div>
-
-            <div className="form-group">
-                <label>Confirm Password</label>
-                <input type="password" className="form-control" placeholder="Enter password" />
-            </div>
-
-            <div className="form-group">
-                <label>First name</label>
-                <input type="text" className="form-control" />
-            </div>
-
-            <div className="form-group">
-                <label>Last name</label>
-                <input type="text" className="form-control" />
-            </div>
-
-            <div className="form-group">
-                <label>Email address</label>
-                <input type="email" className="form-control" />
-            </div>
-
-            <div className="form-group">
-                <label>Contact Number</label>
-                <input type="tel" className="form-control" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" />
-            </div>
-
-            <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-            <p className="forgot-password text-right">
-                Already registered <a href="#">sign in?</a>
-            </p>
-        </form>
-    );
-
+              <span style={{'marginLeft': '180px'}}>Already registered?  
+                <a href="./login">  sign in</a> 
+                </span>
+              <div class="pagination">
+                  <a class="page-link" href="./TermsAndConditions">❮ Previous</a>
+                  <a class="page-link" href="./CheckConfirmEmail">Next ❯</a>
+                </div>
+                
+                <br/><br/>
+                <br/><br/>
     
+                </div>
+                </div>
+        )
+       
 }
 }
 

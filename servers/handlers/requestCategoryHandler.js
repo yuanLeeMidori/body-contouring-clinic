b0cf7f4cb93b8a10a69f8d51b038be9d8e0f8bdf
Eunbee Kim <dmsql1678@gmail.com>
2021-02-11 20:14:04 -0500

    issue-110-Staff: Delete DB Connection string

--- servers/handlers/requestCategoryHandler.js
+++ servers/handlers/requestCategoryHandler.js
@@ -1,22 +1,28 @@
 const RequestCategory = require('../../models/requestCategory');
 
-
+// Create
 function addNewRequestCategory(res){
-    const requestCategory = new RequestCategory({
-      RequestCategoryName : 'MemberShip'
-    });
 
-    requestCategory
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // var reqCatName = req.query.categoryName;
+  // const requestCategory = new RequestCategory({
+  //   RequestCategoryName : reqCatName
+  // });
 
+  const requestCategory = new RequestCategory({
+    RequestCategoryName : 'MemberShip'
+  });
+
+  requestCategory
+  .save()
+  .then((result) => {
+    res.send(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 }
 
+// Read All
 function viewAllRequestCategories(res){
   RequestCategory.find()
   .then((result) => {
@@ -26,5 +32,18 @@ function viewAllRequestCategories(res){
     console.log(err);
   });
 }
+
+// Read One
+function viewRequestCategory(req, res){
+  RequestCategory.findById(req.params.id)    
+  .then((result) => {
+    res.send(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+
 exports.addNewRequestCategory = addNewRequestCategory;
 exports.viewAllRequestCategories = viewAllRequestCategories;
+exports.viewRequestCategory = viewRequestCategory;

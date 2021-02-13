5059b6fa06ff5756e9e93978331880404b796004
yuanLeeMidori <midori3113133@gmail.com>
2021-02-12 22:08:47 -0500

    fix service.js and serviceCategory.js conflict

--- models/serviceCategory.js
+++ models/serviceCategory.js
@@ -3,17 +3,10 @@ const Schema = mongoose.Schema;
 
 const serviceCategorySchema = new Schema(
   {
-<<<<<<< HEAD
     // _id: {
     //   type: mongoose.Types.ObjectId,
     //   required: true,
     // },
-=======
-    _id: {
-      type: Number,
-      required: true,
-    },
->>>>>>> d34c14870e205438315e371742e6bd262d7d764c
     CategoryName: {
       type: String,
       required: true,

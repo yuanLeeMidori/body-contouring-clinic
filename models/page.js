08a2e14391fcb2084fdf1d823d247dd415c2bf25
Eunbee Kim <dmsql1678@gmail.com>
2021-02-13 22:35:56 -0500

    issue-129: wrong FK exists

--- models/page.js
+++ models/page.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const pageSchema = new Schema({
-    accountId: {
-        type: Schema.Types.ObjectId,
-        ref: 'accounts',
-        required: true,
-    },
     isActive: {
         type: Boolean,
         required: true,

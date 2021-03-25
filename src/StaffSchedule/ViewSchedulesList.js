0290138f1be492f91479ec1bbfe225cfcec4aded
yuanLeeMidori <midori3113133@gmail.com>
2021-03-24 23:21:28 -0400

    Add filter for schedule list

--- src/StaffSchedule/ViewSchedulesList.js
+++ src/StaffSchedule/ViewSchedulesList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 import SideBar from '../SideBar/SideBar';
 import { Button, Container, Form, Row, Col } from 'react-bootstrap';
+import searchIcon from '../resources/searchIcon.png';
 import moment from 'moment';
 
 class ViewSchedulesList extends React.Component {
@@ -21,6 +22,8 @@ class ViewSchedulesList extends React.Component {
       workSchedules: [],
       date: [],
       time: [],
+      filter: '',
+      filteredSchedules: [],
     };
   }
 
@@ -31,8 +34,52 @@ class ViewSchedulesList extends React.Component {
       .then((data) => {
         this.setState({
           workSchedules: data,
+          filteredSchedules: data,
         });
       });
+  }
+
+  handleFilterChange(e) {
+    console.log(e.target.value);
+    this.setState({ filter: e.target.value });
+  }
+
+  filterSchedules() {
+    if (this.state.filter == 'today') {
+      const newSchedules = this.state.workSchedules.filter((sch) => {
+        return moment(sch.date.date).isSame(new Date(), 'day');
+      });
+      this.setState({ filteredSchedules: newSchedules });
+    } else if (this.state.filter == 'tomorrow') {
+      const newSchedules = this.state.workSchedules.filter((sch) => {
+        return moment(sch.date.date).isSame(moment().add(1, 'day'), 'day');
+      });
+      this.setState({ filteredSchedules: newSchedules });
+    } else if (this.state.filter == 'week') {
+      const newSchedules = this.state.workSchedules.filter((sch) => {
+        return moment(sch.date.date).isSame(new Date(), 'isoWeek');
+      });
+      this.setState({ filteredSchedules: newSchedules });
+    } else if (this.state.filter == 'nextWeek') {
+      const dayInNextWeek = moment().startOf('isoWeek').add(7, 'day');
+      const newSchedules = this.state.workSchedules.filter((sch) => {
+        return moment(sch.date.date).isSame(dayInNextWeek, 'isoWeek');
+      });
+      this.setState({ filteredSchedules: newSchedules });
+    } else if (this.state.filter == 'month') {
+      const newSchedules = this.state.workSchedules.filter((sch) => {
+        return moment(sch.date.date).isSame(new Date(), 'month');
+      });
+      this.setState({ filteredSchedules: newSchedules });
+    } else if (this.state.filter == 'nextMonth') {
+      const dayInNextMonth = moment().add(1, 'month');
+      const newSchedules = this.state.workSchedules.filter((sch) => {
+        return moment(sch.date.date).isSame(dayInNextMonth, 'month');
+      });
+      this.setState({ filteredSchedules: newSchedules });
+    } else if (this.state.filter == 'all') {
+      this.setState({ filteredSchedules: this.state.workSchedules });
+    }
   }
 
   componentDidMount() {
@@ -43,34 +90,45 @@ class ViewSchedulesList extends React.Component {
           staff: data,
           account: data.account,
         });
-        console.log(this.state.staff);
         this.getWorkSchedules(this.state.staff._id);
       });
   }
 
   render() {
-    console.log(this.state.workSchedules);
     if (this.state.loaded) {
       return (
         <div className="row">
           <div className="col-md-1"></div>
           <SideBar items={this.state.items} />
           <div className="col-md-8" style={{ 'margin-left': '80px' }}>
-            <h2 className="PageTitle">Hi, {this.state.account.firstName + ' ' + this.state.account.lastName}, these are your schedules</h2>
+            <h2 className="PageTitle">
+              Hi, {this.state.account.firstName + ' ' + this.state.account.lastName}, these are your
+              schedules
+            </h2>
             <br />
             <div className="contents">
               <Form inline>
                 <Form.Control
                   as="select"
                   name="days"
-                  value={this.state.dayValue}
-                  onChange={this.handleDayChange}
+                  value={this.state.filter}
+                  onChange={this.handleFilterChange.bind(this)}
                 >
-                  <option value="0">N/A</option>
-                  <option value="30">This Week</option>
-                  <option value="60">This Month</option>
-                  <option value="90">Next Month</option>
+                  <option value="all">N/A</option>
+                  <option value="today">Today</option>
+                  <option value="tomorrow">Tomorrow</option>
+                  <option value="week">This Week</option>
+                  <option value="nextWeek">Next Week</option>
+                  <option value="month">This Month</option>
+                  <option value="nextMonth">Next Month</option>
                 </Form.Control>
+                <Button
+                  variant="outline-*"
+                  onClick={this.filterSchedules.bind(this)}
+                  style={{ background: 'none', 'margin-left': '5px' }}
+                >
+                  <img src={searchIcon} alt="Search" />
+                </Button>
               </Form>
               <br />
               <table>
@@ -79,12 +137,15 @@ class ViewSchedulesList extends React.Component {
                   <th>Time</th>
                 </tr>
 
-                {this.state.workSchedules.map((sch) => (
+                {this.state.filteredSchedules.map((sch) => (
                   <tr key={sch._id}>
                     <td>{moment(sch.date.date).format('ll')}</td>
                     <td>{sch.time.time}</td>
                     <td>
-                      <Button variant="outline-secondary" href={`/Staff/Schedule/Detail/${sch._id}`}>
+                      <Button
+                        variant="outline-secondary"
+                        href={`/Staff/Schedule/Detail/${sch._id}`}
+                      >
                         Details
                       </Button>{' '}
                       <Button variant="outline-info" href={`/Staff/Schedule/Edit/${sch._id}`}>
@@ -101,7 +162,7 @@ class ViewSchedulesList extends React.Component {
                   <Col xs={1}>
                     <Button variant="outline-info" href="/Staff/Schedule/Create">
                       Create
-                  </Button>
+                    </Button>
                   </Col>
                 </Row>
               </Container>

abe35867e39efdf76447ff6ac20f73d66ca973fd
Yuan-Hsi Lee <50813726+yuanLeeMidori@users.noreply.github.com>
2021-04-05 02:07:15 -0400

    Staff can add notes to customer (#378)
    * Add note to customer model
    
    * Complete add notes feature

--- src/Customer/Admin/CustomerProfileAdmin.js
+++ src/Customer/Admin/CustomerProfileAdmin.js
@@ -11,9 +11,10 @@ class CustomerProfileAdmin extends React.Component {
     this.state = {
       account: [],
       accountLevel: [],
-      items: [{ url: `/Customer/Admin`, title: 'Home' },
-              { url: `/Staff/Admin`, title: 'Staff Management' },
-              { url: `/Customer/Admin/Balance`, title: 'Balance Management' },
+      items: [
+        { url: `/Customer/Admin`, title: 'Home' },
+        { url: `/Staff/Admin`, title: 'Staff Management' },
+        { url: `/Customer/Admin/Balance`, title: 'Balance Management' },
       ],
       accountLevels: [],
       balanceHistory: [],
@@ -22,31 +23,41 @@ class CustomerProfileAdmin extends React.Component {
     };
   }
 
-  onAccountLevelChange(event){
+  onAccountLevelChange(event) {
     console.log(this.state.accountLevel);
-    console.log("id: "+event.target.value);
+    console.log('id: ' + event.target.value);
     this.setState(() => ({
-      account:{
+      account: {
         ...this.state.account,
         accountLevelId: event.target.value,
       },
       accountLevel: event.target.value,
     }));
+  }
 
+  onNotesChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      account: {
+        ...this.state.account,
+        notes: event.target.value,
+      },
+    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    fetch(`${process.env.REACT_APP_API_URL}/account/${this.props.id}`,{
-      method: "PUT",
+    fetch(`${process.env.REACT_APP_API_URL}/account/${this.props.id}`, {
+      method: 'PUT',
       body: JSON.stringify(this.state.account),
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },})
-    .then((response) => (response.json()))
-    .then(()=> this.setState({completed: true}))
-    .catch((err) => (console.log(err)));
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then(() => this.setState({ completed: true }))
+      .catch((err) => console.log(err));
   }
 
   getBalanceHistory(id) {
@@ -68,7 +79,7 @@ class CustomerProfileAdmin extends React.Component {
         });
     });
   }
-  getAccountLevels(){
+  getAccountLevels() {
     return new Promise((resolve) => {
       fetch(`${process.env.REACT_APP_API_URL}/account-levels`)
         .then((response) => response.json())
@@ -79,58 +90,66 @@ class CustomerProfileAdmin extends React.Component {
   }
 
   componentDidMount() {
-    this.getCustomerProfile(this.state._id).then((data)=>{
+    this.getCustomerProfile(this.state._id).then((data) => {
       this.setState({
         authName: data.accountLevelId,
       });
     });
     this.getCustomerProfile(this.props.id).then((data) => {
+      console.log(data);
       this.setState({
         account: data,
         accountLevel: data.accountLevelId,
       });
-      this.getBalanceHistory(data.balanceHistory._id)
-      .then((data)=>{
+      this.getBalanceHistory(data.balanceHistory._id).then((data) => {
         this.setState({
           balanceHistory: data,
         });
       });
     });
 
-    this.getAccountLevels()
-    .then((data)=>{
+    this.getAccountLevels().then((data) => {
       this.setState({
         accountLevels: data,
       });
     });
-    
   }
 
   render() {
-    if(this.state.authName == null || this.state.authName._id == '60371ad3fda1af6510e75e3a' || this.state.authName._id == '60371ae9fda1af6510e75e3b')
-    {
-      return (
-        <Redirect push to={{pathname: '/', }}  refresh="true"/>
-      );
+    console.log(this.state.account.notes);
+    if (
+      this.state.authName == null ||
+      this.state.authName._id == '60371ad3fda1af6510e75e3a' ||
+      this.state.authName._id == '60371ae9fda1af6510e75e3b'
+    ) {
+      return <Redirect push to={{ pathname: '/' }} refresh="true" />;
     }
 
-    if(this.state.completed)
-    {
-      return <Redirect push to={{
-        pathname: `/Customer/Admin`
-      }}/>
+    if (this.state.completed) {
+      return (
+        <Redirect
+          push
+          to={{
+            pathname: `/Customer/Admin`,
+          }}
+        />
+      );
     }
     return (
       <div className="row">
         <div className="col-md-1"></div>
         <SideBar items={this.state.items} />
         <div className="col-md-6" style={{ 'margin-left': '80px' }}>
-          <h2 className="PageTitle">Customer : {this.state.account.firstName} {this.state.account.lastName}
+          <h2 className="PageTitle">
+            Customer : {this.state.account.firstName} {this.state.account.lastName}
           </h2>
           <hr />
           <br />
           <Container class="col-md-6">
-            <Form style={{ fontSize: '20px', marginLeft: '80px', textAlign: 'left' }} onSubmit={this.handleSubmit.bind(this)}>
+            <Form
+              style={{ fontSize: '20px', marginLeft: '80px', textAlign: 'left' }}
+              onSubmit={this.handleSubmit.bind(this)}
+            >
               <Form.Group as={Row}>
                 <Form.Label column md={3}>
                   Name:
@@ -146,9 +165,7 @@ class CustomerProfileAdmin extends React.Component {
                   Email:
                 </Form.Label>
                 <Col sm={8}>
-                  <Form.Label>
-                    {this.state.account.email}
-                  </Form.Label>
+                  <Form.Label>{this.state.account.email}</Form.Label>
                 </Col>
               </Form.Group>
               <Form.Group as={Row}>
@@ -156,9 +173,7 @@ class CustomerProfileAdmin extends React.Component {
                   Address:
                 </Form.Label>
                 <Col sm={8}>
-                  <Form.Label >
-                    {this.state.account.address}
-                  </Form.Label>
+                  <Form.Label>{this.state.account.address}</Form.Label>
                 </Col>
               </Form.Group>
               <Form.Group as={Row}>
@@ -166,9 +181,7 @@ class CustomerProfileAdmin extends React.Component {
                   Current Balance:
                 </Form.Label>
                 <Col sm={8}>
-                  <Form.Label>
-                   $ {this.state.balanceHistory.currentBalance}
-                  </Form.Label>
+                  <Form.Label>$ {this.state.balanceHistory.currentBalance}</Form.Label>
                 </Col>
               </Form.Group>
               <Form.Group as={Row}>
@@ -176,15 +189,34 @@ class CustomerProfileAdmin extends React.Component {
                   Customer level:
                 </Form.Label>
                 <Col sm={4}>
-                  <Form.Control as="select" controlId="accountLevelId" value={this.state.accountLevel == null ? '':this.state.accountLevel._id} onChange={this.onAccountLevelChange.bind(this)}>
-                    {this.state.accountLevels.map((level)=>(
-                      level.name !="Staff"?<option key={level._id} value={level._id}>{level.name}</option>:""
-                    ))}
+                  <Form.Control
+                    as="select"
+                    controlId="accountLevelId"
+                    value={this.state.accountLevel == null ? '' : this.state.accountLevel._id}
+                    onChange={this.onAccountLevelChange.bind(this)}
+                  >
+                    {this.state.accountLevels.map((level) =>
+                      level.name != 'Staff' ? (
+                        <option key={level._id} value={level._id}>
+                          {level.name}
+                        </option>
+                      ) : (
+                        ''
+                      )
+                    )}
                   </Form.Control>
                 </Col>
               </Form.Group>
+              <Form.Group>
+                <Form.Label>Notes:</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  value={this.state.account.notes == null ? '' : this.state.account.notes}
+                  onChange={this.onNotesChange.bind(this)}
+                ></Form.Control>
+              </Form.Group>
               <Button type="submit" variant="outline-info">
-                  Edit
+                Confirm Changes
               </Button>
             </Form>
           </Container>
